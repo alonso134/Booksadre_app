fc98fe447aef5b06e8330f5d6f61f4cd084432f0
Alonso Moreno <20220664@ricaldone.edu.sv>
2024-07-05 13:41:50 -0600

    Comienzo de la app movil

---
+++ BooksadreApp/src/screens/perfil.js
@@ -0,0 +1,192 @@
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { Appbar } from 'react-native-paper';
+
+const Perfil = ({ navigation }) => {
+
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => setMenuVisible(!menuVisible);
+
+  return (
+    <View style={styles.container}>
+      <Appbar.Header style={styles.appBar}>
+        <Appbar.Content title="CECSL" titleStyle={styles.appBarTitle} />
+        <TouchableOpacity onPress={toggleMenu} style={styles.menuButton}>
+          <Image source={require('../../assets/menu-icon.png')} style={styles.menuIcon} />
+        </TouchableOpacity>
+      </Appbar.Header>
+
+      <View style={styles.profileContainer}>
+        <Text style={styles.profileTitle}>Perfil</Text>
+        <View style={styles.profileInfo}>
+          <Image source={require('../../assets/avatar.png')} style={styles.profileImage} />
+          <Text style={styles.profileId}>20220133</Text>
+          <Text style={styles.profileName}>Fernando Alonso Martínez Rosales</Text>
+          <Text style={styles.profileGrade}>Noveno Grado</Text>
+          <TouchableOpacity style={styles.logoutButton}>
+            <Text style={styles.logoutButtonText}>Cerrar Sesión</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {menuVisible && (
+        <View style={styles.overlay}>
+          <View style={styles.menu}>
+            <TouchableOpacity
+              style={styles.menuItem}
+              onPress={() => navigation.navigate('Home')}>
+              <Text>Inicio</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.menuItem}
+              onPress={() => navigation.navigate('MateriasScreen')}>
+              <Text>Materias</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.menuItem}
+              onPress={() => navigation.navigate('Perfil')}>
+              <Text>Perfil</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.menuItem}
+              onPress={() => navigation.navigate('Observaciones')}>
+              <Text>Observaciones</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.menuItem}
+              onPress={() => navigation.navigate('Ausencias')}>
+              <Text>Ausencias</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.menuItem}
+              onPress={() => navigation.navigate('Tarde')}>
+              <Text>Llegadas Tarde</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.menuItem} onPress={() => navigation.navigate('Codigos')}>
+              <Text>Codigos</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.closeButton} onPress={toggleMenu}>
+              <Text style={styles.closeButtonText}>Cerrar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F0FFFF',
+  },
+  appBar: {
+    backgroundColor: '#120851',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    elevation: 0,
+  },
+  appBarTitle: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  menuButton: {
+    padding: 10,
+    marginRight: 10,
+  },
+  menuIcon: {
+    width: 35,
+    height: 35,
+  },
+  profileContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  profileTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  profileInfo: {
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    padding: 20,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  profileImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    marginBottom: 10,
+  },
+  verifiedIcon: {
+    width: 24,
+    height: 24,
+    position: 'absolute',
+    top: 70,
+    right: 10,
+  },
+  profileId: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  profileName: {
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  profileGrade: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  logoutButton: {
+    backgroundColor: '#000080',
+    padding: 10,
+    borderRadius: 5,
+  },
+  logoutButtonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  menu: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 10,
+    padding: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  menuItem: {
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#CCCCCC',
+  },
+  closeButton: {
+    marginTop: 10,
+    alignItems: 'center',
+  },
+  closeButtonText: {
+    color: 'red',
+    fontSize: 16,
+  },
+});
+
+export default Perfil;
